359aa4b8e22482e4633bec3b146da5c00ac54cb1
LionThrewpy <karper360l@gmail.com>
2020-05-16 22:04:46 +0100

    Refactored to include methods that UserStore needs

--- src/Infrastructure/MovieCRUD.Infrastructure.Persistence/Services/Interfaces/IUserRepository.cs
+++ src/Infrastructure/MovieCRUD.Infrastructure.Persistence/Services/Interfaces/IUserRepository.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNet.Identity;
-using MovieCRUD.Domain.Authentication;
+﻿using MovieCRUD.Domain.Authentication;
+using MovieCRUD.Domain.Authentication.ValueObjects;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -7,22 +7,10 @@ namespace MovieCRUD.Infrastructure.Persistence.Interfaces
 {
     public interface IUserRepository : IRepository<User>
     {
-        Task<IdentityResult> RegisterUserAsync(User user);
-        Task<User> GetUserAsync(string userName, string password);
-        Task<User> GetUserByIdAsync(string userId);
-        Task<User> GetExternalUserAsync(UserLoginInfo loginInfo);
-        Task<IdentityResult> RemovePasswordAsync(string userId);
-        Task<IdentityResult> RemoveLoginAsync(string userId, UserLoginInfo loginInfo);
-        Task<IdentityResult> AddLoginAsync(string userId, UserLoginInfo loginInfo);
-        Task<IdentityResult> AddPasswordAsync(string userId, string newPassword);
-        Task<IdentityResult> ChangePasswordAsync(string userId, string oldPassword, string newPassword);
-        Task<string> GenerateChangePhoneNumberToken(string userId, string phoneNumber);
-        Task<IdentityResult> SetTwoFactorEnabled(string userId, bool isEnabled);
-        Task<IdentityResult> ChangePhoneNumberAsync(string userId, string phoneNumber, string token);
-        Task<string> GetPhoneNumberAsync(string userId);
-        Task<IdentityResult> SetPhoneNumberAsync(string userId, string newNumber);
-        Task<bool> GetTwoFactorEnabledAsync(string userId);
-        Task<IList<UserLoginInfo>> GetLoginsAsync(string userId);
-        Task SignIn(User user, bool isPersistent, bool rememberBrowser);
+        Task<User> GetUserByNameAsync(string userName);
+        Task<User> GetUserByExternalLoginInfoAsync(UserLoginData loginInfo);
+        Task<IList<Claim>> GetClaimsAsync(int userId);
+        Task<IList<UserLoginData>> GetLoginsAsync(int userId);
+        Task GetPasswordHashAsync(int userId);
     }
 }
