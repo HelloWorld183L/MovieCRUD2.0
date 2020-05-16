eea6ef2cb01c5675497f668a1b4ddc53d23bb235
LionThrewpy <karper360l@gmail.com>
2020-05-16 22:01:35 +0100

    Added Logins and Claims properties

--- src/Authentication/MovieCRUD.Domain.Authentication/Entities/User.cs
+++ src/Authentication/MovieCRUD.Domain.Authentication/Entities/User.cs
@@ -1,4 +1,6 @@
-﻿using MovieCRUD.SharedKernel;
+﻿using MovieCRUD.Domain.Authentication.ValueObjects;
+using MovieCRUD.SharedKernel;
+using System.Collections.Generic;
 
 namespace MovieCRUD.Domain.Authentication
 {
@@ -6,8 +8,10 @@ namespace MovieCRUD.Domain.Authentication
     {
         public int Id { get; set; }
         public string Username { get; set; }
-        public string Password { get; set; }
         public string PasswordHash { get; set; }
         public string PhoneNumber { get; set; }
+
+        public ICollection<UserLoginData> Logins;
+        public ICollection<Claim> Claims { get; set; }
     }
 }
