fa0feed6ca0b21fa6a5454aad8b26a1f611d33fa
LionThrewpy <karper360l@gmail.com>
2020-05-18 21:07:34 +0100

    Added mapping profile + UserLoginData entity

---
+++ src/Authentication/MovieCRUD.Authentication/MappingProfiles/ClaimsProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using System.Security.Claims;
+
+namespace MovieCRUD.Authentication.MappingProfiles
+{
+    public class ClaimsProfile : Profile
+    {
+        public ClaimsProfile()
+        {
+            CreateMap<Claim, Domain.Authentication.Claim>().ReverseMap();
+        }
+    }
+}
---
+++ src/Authentication/MovieCRUD.Domain.Authentication/Entities/UserLoginData.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieCRUD.Domain.Authentication
+{
+    public class UserLoginData
+    {
+        public string LoginProvider { get; set; }
+        public string ProviderKey { get; set; }
+    }
+}
