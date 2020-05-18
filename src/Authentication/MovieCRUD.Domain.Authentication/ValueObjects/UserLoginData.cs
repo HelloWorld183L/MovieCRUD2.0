7b472cd68234ef9d9d227ec180b57d2fb8a6fee9
LionThrewpy <karper360l@gmail.com>
2020-05-18 21:08:43 +0100

    Deleted ValueObjects folder

--- src/Authentication/MovieCRUD.Domain.Authentication/ValueObjects/UserLoginData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MovieCRUD.Domain.Authentication.ValueObjects
-{
-    public class UserLoginData
-    {
-        public string LoginProvider { get; set; }
-        public string ProviderKey { get; set; }
-    }
-}
