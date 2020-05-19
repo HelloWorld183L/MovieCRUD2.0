9ff7897c53ee2672f6efbc22d95339945513fc52
Leon Harper <karper360l@gmail.com>
2020-05-19 18:17:34 +0100

    Added GenerateUserIdentityAsync method

--- src/Authentication/MovieCRUD.Authentication/Models/IdentityModels/UserDTO.cs
+++ src/Authentication/MovieCRUD.Authentication/Models/IdentityModels/UserDTO.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNet.Identity;
 using MovieCRUD.Domain.Authentication;
 using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace MovieCRUD.Authentication.Models.IdentityModels
 {
@@ -10,5 +12,13 @@ namespace MovieCRUD.Authentication.Models.IdentityModels
         public string UserName { get; set; }
         public string PasswordHash { get; set; }
         public ICollection<ExternalLogin> Logins { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<UserDTO, int> manager, string authenticationType)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+        }
     }
 }
