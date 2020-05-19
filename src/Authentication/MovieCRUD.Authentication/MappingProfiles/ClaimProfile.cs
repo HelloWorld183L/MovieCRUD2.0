8196a4ddf547c922bd11a23e5383729eb9706aea
Leon Harper <karper360l@gmail.com>
2020-05-19 18:37:56 +0100

    Added domain object mapping profiles

---
+++ src/Authentication/MovieCRUD.Authentication/MappingProfiles/ClaimProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using System.Security.Claims;
+
+namespace MovieCRUD.Authentication.MappingProfiles
+{
+    public class ClaimProfile : Profile
+    {
+        public ClaimProfile()
+        {
+            CreateMap<Domain.Authentication.Claim, Claim>().ReverseMap();
+        }
+    }
+}
---
+++ src/Authentication/MovieCRUD.Authentication/MappingProfiles/LoginInfoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Microsoft.AspNet.Identity;
+using MovieCRUD.Authentication.Models;
+
+namespace MovieCRUD.Authentication.MappingProfiles
+{
+    public class LoginInfoProfile : Profile
+    {
+        public LoginInfoProfile()
+        {
+            CreateMap<UserLoginInfo, ExternalLoginData>().ReverseMap();
+        }
+    }
+}
