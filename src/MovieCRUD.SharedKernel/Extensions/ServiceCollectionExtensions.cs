1b96fcca30330526d3f50bb65cf12a588e38a736
Leon Harper <karper360l@gmail.com>
2020-05-21 21:41:48 +0100

    Renamed InstallerExtensions to ServiceCollectionExtensions

---
+++ src/MovieCRUD.SharedKernel/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace MovieCRUD.SharedKernel
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void InstallTypesInAssembly(this IServiceCollection services)
+        {
+            var installers = AppDomain
+                .CurrentDomain
+                .GetAssemblies()
+                .SelectMany(assembly => assembly.GetTypes())
+                .Where(type => type.GetInterfaces().Contains(typeof(IInstaller)))
+                .Select(Activator.CreateInstance)
+                .Cast<IInstaller>()
+                .ToList();
+
+            installers.ForEach(installer => installer.InstallTypes(services));
+        }
+    }
+}
