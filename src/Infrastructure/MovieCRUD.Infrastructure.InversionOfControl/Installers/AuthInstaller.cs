4399a880f8e4d0359bd6b082c7066a55a120daff
Leon Harper <karper360l@gmail.com>
2020-05-21 12:36:17 +0100

    Removed useless directives

--- src/Infrastructure/MovieCRUD.Infrastructure.InversionOfControl/Installers/AuthInstaller.cs
+++ src/Infrastructure/MovieCRUD.Infrastructure.InversionOfControl/Installers/AuthInstaller.cs
@@ -1,15 +1,9 @@
-﻿using Autofac;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using MovieCRUD.Authentication.Clients;
 using MovieCRUD.Infrastructure.InversionOfControl.Installers.Interfaces;
 using MovieCRUD.Infrastructure.Logging;
 using MovieCRUD.Infrastructure.Persistence.Interfaces;
 using MovieCRUD.Infrastructure.Persistence.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MovieCRUD.Infrastructure.InversionOfControl.Installers
 {
@@ -18,6 +12,7 @@ namespace MovieCRUD.Infrastructure.InversionOfControl.Installers
         public void InstallTypes(IServiceCollection services)
         {
             var logger = new Logger();
+
             services.AddTransient<IUserRepository, UserRepository>()
                     .AddSingleton<IAuthApiClient, AuthApiClient>(provider =>
                     {
